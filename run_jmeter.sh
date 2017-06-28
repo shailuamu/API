04e91bc63c9803d1d1dad99eab597d4ecac2ce0a
Chintamani Manjare <ChintamaniM@talentica-all.com>
2017-06-28 22:52:03 +0530

    changes

--- run_jmeter.sh
+++ run_jmeter.sh
@@ -4,4 +4,8 @@ ls /var/go/apache-jmeter-3.12/bin
 
 echo "Run Jmeter command"
 
+echo "current folder"
+
+ls
+
 /var/go/apache-jmeter-3.12/bin/jmeter.sh -nt REST_Framework.jmx -l ./Rest_Framework -eo ./Rest_Framework_Report
